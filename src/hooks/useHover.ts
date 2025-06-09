253720f6bf92cb99cd3e4dbb74bff869feaab1f3
dmkt9 <demoskratos9x@gmail.com>
2025-06-09 11:30:52 +0700

    Fix - Report preview remain highlighted when closed using device´s back button

--- src/hooks/useHover.ts
+++ src/hooks/useHover.ts
@@ -1,7 +1,15 @@
-import {useState} from 'react';
+import {useIsFocused} from '@react-navigation/native';
+import {useEffect, useState} from 'react';
 
 const useHover = () => {
     const [hovered, setHovered] = useState(false);
+    const isFocused = useIsFocused();
+    // eslint-disable-next-line rulesdir/prefer-early-return
+    useEffect(() => {
+        if (!isFocused) {
+            setHovered(false);
+        }
+    }, [isFocused]);
     return {
         hovered,
         bind: {
