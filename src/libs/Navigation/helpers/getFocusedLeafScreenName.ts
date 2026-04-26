16f431882e0887177c38e6d95b06341fe3957453
dmkt9 <demoskratos9x@gmail.com>
2026-04-26 22:14:44 +0700

    fix: tab navigation bar appears incorrectly when opening via deep link

--- src/libs/Navigation/helpers/getFocusedLeafScreenName.ts
+++ src/libs/Navigation/helpers/getFocusedLeafScreenName.ts
@@ -4,10 +4,13 @@ import type {NavigationState, PartialState} from '@react-navigation/native';
  * Walks down the focused route chain and returns the leaf screen name.
  */
 function getFocusedLeafScreenName(state: NavigationState | PartialState<NavigationState> | undefined): string | undefined {
-    if (!state || state.index === undefined) {
+    // When opening the app via deeplink, the state isn't fully hydrated and only contains `routes` and `stale` properties.
+    // In this case, we still rely on `focused?.name` to prevent the `TabNavigationBar` from showing up immediately upon app launch.
+    if (!state || (state.index === undefined && !(Object.values(state).length === 2 && state.routes.length === 1))) {
         return undefined;
     }
-    const focused = state.routes[state.index];
+
+    const focused = state.routes[state.index ?? 0];
     if (focused?.state) {
         return getFocusedLeafScreenName(focused.state);
     }
